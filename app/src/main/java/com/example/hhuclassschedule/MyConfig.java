6d92a81ca5d71c9d10b5c037b957088767eb8b3c
yuchenii <dev.chenyu@gmail.com>
2021-06-08 21:51:19 +0800

    保存配置到本地时使用封装的SharedPreferencesUtil

--- app/src/main/java/com/example/hhuclassschedule/MyConfig.java
+++ app/src/main/java/com/example/hhuclassschedule/MyConfig.java
@@ -3,33 +3,54 @@ package com.example.hhuclassschedule;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import com.example.hhuclassschedule.util.ContextApplication;
+import com.example.hhuclassschedule.util.SharedPreferencesUtil;
+
 import java.util.Map;
 import java.util.Set;
 
 public class MyConfig {
     public static final String TAG = "MyConfig";
-    private Context myContext;
-    public MyConfig(Context myContext) {
-        this.myContext = myContext;
-    }
+//    private Context myContext;
+//    public MyConfig(Context myContext) {
+//        this.myContext = myContext;
+//    }
 
     //保存当前配置信息（缓冲map）至本地文件
-    public void saveConfig(Map<String, String> configMap){
-        SharedPreferences.Editor editor =
-                myContext.getSharedPreferences(MainActivity.CONFIG_FILENAME,Context.MODE_PRIVATE).edit();
-        for(String key : configMap.keySet()){
+    public void saveConfig(Map<String, String> configMap) {
+        SharedPreferencesUtil sharedPreferencesUtil =
+                SharedPreferencesUtil.init(ContextApplication.getAppContext(), MainActivity.CONFIG_FILENAME);
+        for (String key : configMap.keySet()) {
             String value = configMap.get(key);
-            editor.putString(key, value);
-            editor.commit();
+            sharedPreferencesUtil.putString(key, value);
         }
     }
 
+
+//    public void saveConfig(Map<String, String> configMap){
+//        SharedPreferences.Editor editor =
+//                myContext.getSharedPreferences(MainActivity.CONFIG_FILENAME,Context.MODE_PRIVATE).edit();
+//        for(String key : configMap.keySet()){
+//            String value = configMap.get(key);
+//            editor.putString(key, value);
+//            editor.commit();
+//        }
+//    }
+
     //从本地配置文件中读取信息至缓冲map
-    public Map<String, String> loadConfig(){
+    public Map<String, String> loadConfig() {
         Map<String, String> configMap;
-        SharedPreferences sharedPreferences =
-                myContext.getSharedPreferences(MainActivity.CONFIG_FILENAME,Context.MODE_PRIVATE);
-        configMap = (Map<String, String>) sharedPreferences.getAll();
+        SharedPreferencesUtil sharedPreferencesUtil =
+                SharedPreferencesUtil.init(ContextApplication.getAppContext(), MainActivity.CONFIG_FILENAME);
+        configMap = (Map<String, String>) sharedPreferencesUtil.getAll();
         return configMap;
     }
+
+//    public Map<String, String> loadConfig(){
+//        Map<String, String> configMap;
+//        SharedPreferences sharedPreferences =
+//                myContext.getSharedPreferences(MainActivity.CONFIG_FILENAME,Context.MODE_PRIVATE);
+//        configMap = (Map<String, String>) sharedPreferences.getAll();
+//        return configMap;
+//    }
 }
