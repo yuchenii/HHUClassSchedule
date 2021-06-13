b393577d95ba34c337e0a989ded5cb4d35eba5f7
Chaldean <1446334204@qq.com>
2021-06-13 14:35:59 +0800

    加入了通知相关的变量

--- app/src/main/java/com/example/hhuclassschedule/adapter/OnMyConfigHandleAdapter.java
+++ app/src/main/java/com/example/hhuclassschedule/adapter/OnMyConfigHandleAdapter.java
@@ -14,8 +14,17 @@ public class OnMyConfigHandleAdapter {
 
     public static final String CONFIG_SHOW_WEEKENDS = "config_show_weekends";
     public static final String CONFIG_SHOW_NOT_CUR_WEEK = "config_show_not_cur_week";
+    /**
+     * 存储的是开学日期，需利用其他工具动态计算当前周;
+     * 存储格式"yy-MM-dd HH:mm:ss"
+     */
     public static final String CONFIG_CUR_WEEK = "config_current_week";
     public static final String CONFIG_SHOW_TIME = "config_show_time";
+    public static final String CONFIG_NOT_OPEN = "config_not_open";
+    public static final String CONFIG_NOT_SHOW_WHERE = "config_not_where";
+    public static final String CONFIG_NOT_SHOW_WHEN = "config_not_when";
+    public static final String CONFIG_NOT_SHOW_STEP = "config_not_step";
+
 
     public static final String VALUE_TRUE = "config_value_true";
     public static final String VALUE_FALSE = "config_value_false";
